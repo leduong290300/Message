813f1e37d6946d76fd5388ae36f871f23fb4079c
leduong290300 <leduong290300@gmail.com>
2022-01-05 16:56:14 +0700

    Design-Login

---
+++ app/src/Pages/Login/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Login.css";
+import LoginForm from "../../Components/LoginForm/LoginForm";
+export default function Login() {
+  return (
+    <div id="login">
+      <LoginForm />
+    </div>
+  );
+}
