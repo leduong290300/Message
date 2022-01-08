d88fed00ddd9627081fbe6c543be1ac55551e41a
leduong290300 <leduong290300@gmail.com>
2022-01-08 15:10:13 +0700

    Send-message-v1

--- app/src/Components/MessageList/MessageList.js
+++ app/src/Components/MessageList/MessageList.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { Avatar, Typography } from "antd";
 import { Container } from "./Styles/MessageListStyle";
+import { formatRelative } from "date-fns/esm";
+function formatDate(seconds) {
+  let format = "";
+  if (seconds) {
+    format = formatRelative(new Date(seconds * 1000), new Date());
+    format = format.charAt(0).toUpperCase() + format.slice(1);
+  }
+  return format;
+}
 export default function MessageList({
   message,
   displayName,
@@ -11,10 +20,12 @@ export default function MessageList({
     <Container>
       <div>
         <Avatar size="small" src={photoURL}>
-          A
+          {photoURL ? "" : displayName?.charAt(0)?.toUpperCase()}
         </Avatar>
         <Typography.Text className="author">{displayName}</Typography.Text>
-        <Typography.Text className="time">{createdAt}</Typography.Text>
+        <Typography.Text className="time">
+          {formatDate(createdAt?.seconds)}
+        </Typography.Text>
       </div>
       <div>
         <Typography.Text className="content">{message}</Typography.Text>
